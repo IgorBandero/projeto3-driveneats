120b08d6566932268c6d96109c34eb63b7afa9e8
Igor Velmud Bandero <igorbandero@gmail.com>
2023-01-09 12:08:02 -0300

    Botão de fechar pedido pronto

--- script/script.js
+++ script/script.js
@@ -5,5 +5,24 @@ function selecionarOpcao (elemento, tipo){
         selecionado_anteriormente.classList.remove(tipo);        
     }   
     elemento.classList.add(tipo);     
+
+    verificaPedidoCompleto ();
 }
-    
+
+function  verificaPedidoCompleto (){
+    let pratoPrincipal = document.querySelector(".prato-selecionado");
+    let bebida = document.querySelector(".bebida-selecionada");
+    let sobremesa = document.querySelector(".sobremesa-selecionada");
+
+    if ((pratoPrincipal != null)&&(bebida != null)&&(sobremesa != null)){
+        habilitaBotao();
+    }
+}
+
+function habilitaBotao(){
+    let botao = document.querySelector(".botao-pedir");
+    botao.classList.add("botao-habilitado");
+    botao.innerHTML = "Fechar pedido";
+    botao.setAttribute("onclick", "fechaPedido();");
+}
+
