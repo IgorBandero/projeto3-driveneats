f173cc1329fba3a7999775b4ed50a4b7dcc71114
Igor Velmud Bandero <igorbandero@gmail.com>
2023-01-09 16:05:58 -0300

    Bônus finalizado

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ let nomeSobremesa;
 let valorPratoPrincipal;
 let valorBebida;
 let valorSobremesa;
-let total;
+let valorTotal;
 
 function selecionarOpcao (elemento, tipo){
     let selecionado_anteriormente = document.querySelector("." + tipo);
@@ -28,7 +28,7 @@ function  verificaPedidoCompleto (){
     valorPratoPrincipal = parseFloat((document.querySelector(".prato-selecionado .preco-prato").innerHTML).replace(',', '.'));
     valorBebida = parseFloat((document.querySelector(".bebida-selecionada .preco-bebida").innerHTML).replace(',', '.'));
     valorSobremesa = parseFloat((document.querySelector(".sobremesa-selecionada .preco-sobremesa").innerHTML).replace(',', '.'));
-    total = valorPratoPrincipal + valorBebida + valorSobremesa;
+    valorTotal = valorPratoPrincipal + valorBebida + valorSobremesa;
     
     if ((pratoPrincipal != null)&&(bebida != null)&&(sobremesa != null)){
         habilitaBotao();
@@ -40,10 +40,33 @@ function habilitaBotao(){
     botao.classList.add("botao-habilitado");
     botao.innerHTML = "Fechar pedido";
     botao.disabled = false;
-    botao.setAttribute("onclick", "fechaPedido();");
+    botao.setAttribute("onclick", "confirmaPedido();");
 }
 
-function fechaPedido(){
-    var valorTotal = total.toFixed(2);
-    window.open('https://wa.me/5548991712977?text=Olá,%20gostaria%20de%20fazer%20o%20pedido:%0a-%20Prato:%20' + nomePratoPrincipal + '%0a-%20Bebida:%20' + nomeBebida + '%0a-%20Sobremesa:%20' + nomeSobremesa + '%0a%20Total:%20R$%20' + valorTotal);
+function confirmaPedido(){
+    let mensagem = document.querySelector(".tela-confirmacao");
+    
+    document.querySelector(".pratoEscolhido").innerHTML = nomePratoPrincipal;
+    document.querySelector(".bebidaEscolhida").innerHTML = nomeBebida;
+    document.querySelector(".sobremesaEscolhida").innerHTML = nomeSobremesa;
+
+    document.querySelector(".valorPratoEscolhido").innerHTML = valorPratoPrincipal.toFixed(2);
+    document.querySelector(".valorBebidaEscolhida").innerHTML = valorBebida.toFixed(2);
+    document.querySelector(".valorSobremesaEscolhida").innerHTML = valorSobremesa.toFixed(2);
+
+    valorTotal = valorTotal.toFixed(2);
+    valorTotal = valorTotal.replace('.', ',');
+    document.querySelector(".valorTotalPedido").innerHTML = "R$ " + valorTotal;
+
+    mensagem.classList.add("mostrar");
+}
+
+function fechaPedido(){    
+    let nome = prompt("Informe o seu nome: ");
+    let endereco = prompt("Informe o seu endereco: ");
+    window.open('https://wa.me/5548991712977?text=Olá,%20gostaria%20de%20fazer%20o%20pedido:%0a-%20Prato:%20' + nomePratoPrincipal + '%0a-%20Bebida:%20' + nomeBebida + '%0a-%20Sobremesa:%20' + nomeSobremesa + '%0a%20Total:%20R$%20' + valorTotal +"%0a%0aNome:%20" + nome + "%0aEndereço:%20" + endereco);
+}
+
+function cancelarPedido() {
+    window.location.reload(true);
 }
